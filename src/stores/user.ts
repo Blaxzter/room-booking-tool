402738597f0c634d511671411f913cc7addfe8d1
Frederic Abraham <frederic.abraham@respeak.io>
2024-08-20 10:46:28 +0200

    change registration email error and fix top nav on mobile

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -281,7 +281,7 @@ export const useUser = defineStore('user', () => {
       registerUser(data.email, data.password, {
         first_name: data.first_name,
         last_name: data.last_name,
-        verification_url: `https://${window.location.host}/verify-email`
+        verification_url: `${window.location.protocol}//${window.location.host}/verify-email`
       })
     )
   }
