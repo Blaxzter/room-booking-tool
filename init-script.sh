5b4db3e76d91b3f7bd9e95cd3f16a179c95d0e4c
Frederic Abraham <frederic.abraham@respeak.io>
2024-10-29 14:30:53 +0100

    Add the default user settings call

--- init-script.sh
+++ init-script.sh
@@ -26,15 +26,14 @@ if [ "$COLLECTION_EXISTS" = "null" ]; then
     # Run directus-sync push to sync your schema and data
     npx directus-sync@2.2.0 push -u "$HOST_URL" -e "$ADMIN_EMAIL" -p "$ADMIN_PASSWORD"
 
+    # Obtain an admin access token
+    TOKEN=$(curl -s -X POST -d "email=$ADMIN_EMAIL&password=$ADMIN_PASSWORD" \
+      "$HOST_URL/auth/login" | jq -r '.data.access_token')
+
     # Check if DEMO_MODE is enabled
     if [ "$DEMO_MODE" = "true" ]; then
         echo "Demo mode is enabled, setting initial user role to 'demo'"
 
-        # Obtain an admin access token
-        TOKEN=$(curl -s -X POST -d "email=$ADMIN_EMAIL&password=$ADMIN_PASSWORD" \
-          "$HOST_URL/auth/login" | jq -r '.data.access_token')
-
-        # curl http://localhost:8055/roles and get the array element with name "name": "DemoUser", then get the "id" value
         DEMO_ROLE_RESPONSE=$(curl -s -H "Authorization: Bearer $TOKEN" \
                   "$HOST_URL/roles?fields=id&filter[name][_eq]=DemoUser")
 
@@ -46,9 +45,32 @@ if [ "$COLLECTION_EXISTS" = "null" ]; then
         fi
 
         echo "Demo role ID: $DEMO_ROLE_ID"
+        # do a patch  call on {{root}}/settings with
+        # {
+        #     "public_registration_role": "role_id"
+        # }
+        curl -s -X PATCH -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" \
+          -d "{\"public_registration_role\": \"$DEMO_ROLE_ID\"}" \
+          "$HOST_URL/settings"
+    else
+        MEMBER_ROLE_RESPONSE=$(curl -s -H "Authorization: Bearer $TOKEN" \
+                  "$HOST_URL/roles?fields=id&filter[name][_eq]=Member")
 
-        # following set the default user
+        MEMBER_ROLE_ID=$(echo "$MEMBER_ROLE_RESPONSE" | jq -r '.data[0].id')
 
+        if [ -z "$MEMBER_ROLE_ID" ] || [ "$MEMBER_ROLE_ID" = "null" ]; then
+            echo "Role 'Member' not found. Please ensure the role exists."
+            exit 1
+        fi
+
+        echo "Member role ID: $MEMBER_ROLE_ID"
+        # do a patch  call on {{root}}/settings with
+        # {
+        #     "public_registration_role": "role_id"
+        # }
+        curl -s -X PATCH -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" \
+          -d "{\"public_registration_role\": \"$MEMBER_ROLE_ID\"}" \
+          "$HOST_URL/settings"
     fi
 
     echo "Initialization complete."
