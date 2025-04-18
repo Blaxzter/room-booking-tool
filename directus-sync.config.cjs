e26bae73d536ca00d2282d4ad0e5daf3f488f720
Frederic Abraham <mail@fabraham.dev>
2025-04-18 23:33:04 +0200

    fix: remove commented-out additional options from Directus sync configuration

--- directus-sync.config.cjs
+++ directus-sync.config.cjs
@@ -2,5 +2,4 @@ module.exports = {
     directusUrl: 'http://localhost:8055',
     directusToken: 'kFmZZ1iBleIEjVfKzixosDspPka2mAyx',
     dumpPath: './directus-config',
-    // Additional options...
 }; 
